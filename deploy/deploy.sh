7aa7b7f42277fcb2ef5465f7209aa960b02db9cc
Nicholas <nicholas.zhaoyu@gmail.com>
2016-09-05 09:46:09 -0700

    Add 1.2.7 update package

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
-docker build -t gcr.io/nich01as-com/androidota:20160611_01 .
-gcloud docker push gcr.io/nich01as-com/androidota:20160611_01
+docker build -t gcr.io/nich01as-com/androidota:20160905 .
+gcloud docker push gcr.io/nich01as-com/androidota:20160905
 
 
