b609d6c98e333e9d0195783ea3513c03a0a8065f
Nicholas <nicholas.zhaoyu@gmail.com>
2016-12-08 23:44:52 -0800

    Add OTA for RePad10

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,2 @@
-docker build -t gcr.io/nich01as-com/androidota:20160909-2 .
-gcloud docker push gcr.io/nich01as-com/androidota:20160909-2
-
-
+docker build -t gcr.io/nich01as-com/androidota:20161108-1 .
+gcloud docker push gcr.io/nich01as-com/androidota:20161108-1
