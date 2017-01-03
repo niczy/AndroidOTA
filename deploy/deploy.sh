ec4d87845986e5c1f36f474e44b4142c0097495a
Nicholas <nicholas.zhaoyu@gmail.com>
2017-01-03 11:03:40 -0800

    Update config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
-docker build -t gcr.io/nich01as-com/androidota:20161108-1 .
-gcloud docker push gcr.io/nich01as-com/androidota:20161108-1
+docker build -t gcr.io/nich01as-com/androidota:20161119 .
+gcloud docker push gcr.io/nich01as-com/androidota:20161119
